db26ac57373c03b521fc967cfc4fefe5f05ae5f2
kurami <kurami.noya@gmail.com>
2023-06-19 13:46:49 +0800

    feat： 产品管理功能添加1-3

--- src/views/product/Product.data.ts
+++ src/views/product/Product.data.ts
@@ -1,57 +1,55 @@
-import {BasicColumn} from '/@/components/Table';
-import {FormSchema} from '/@/components/Table';
-import { rules} from '/@/utils/helper/validator';
+import { BasicColumn } from '/@/components/Table';
+import { FormSchema } from '/@/components/Table';
+import { rules } from '/@/utils/helper/validator';
 import { render } from '/@/utils/common/renderUtils';
 //列表数据
 export const columns: BasicColumn[] = [
-   {
+  {
     title: '标题',
-    align:"center",
-    dataIndex: 'title'
-   },
-   {
+    align: 'center',
+    dataIndex: 'title',
+  },
+  {
+    title: '编号',
+    align: 'center',
+    dataIndex: 'number',
+  },
+  {
     title: '描述',
-    align:"center",
+    align: 'center',
     dataIndex: 'productDec',
     slots: { customRender: 'htmlSlot' },
-   },
-   {
+  },
+  {
     title: '出发地',
-    align:"center",
+    align: 'center',
     dataIndex: 'departure',
     slots: { customRender: 'pcaSlot' },
-   },
-   {
-    title: '标签',
-    align:"center",
-    dataIndex: 'tags'
-   },
-   {
+  },
+  {
     title: '海报',
-    align:"center",
-    dataIndex: 'img',
-    customRender:render.renderImage,
-   },
-   {
-    title: '行程标题',
-    align:"center",
-    dataIndex: 'journeyTitle'
-   },
-   {
+    align: 'center',
+    dataIndex: 'imgString',
+  },
+  {
     title: '行程描述',
-    align:"center",
+    align: 'center',
     dataIndex: 'journeyDesc',
     slots: { customRender: 'htmlSlot' },
-   },
+  },
 ];
 //查询数据
-export const searchFormSchema: FormSchema[] = [
-];
+export const searchFormSchema: FormSchema[] = [];
 //表单数据
 export const formSchema: FormSchema[] = [
   {
     label: '标题',
     field: 'title',
+    component: 'Input',
+  },
+  {
+    label: '编号',
+    field: 'number',
     component: 'Input',
   },
   {
@@ -65,43 +63,30 @@ export const formSchema: FormSchema[] = [
     component: 'JAreaLinkage',
   },
   {
-    label: '标签',
-    field: 'tags',
-    component: 'Input',
-  },
-  {
     label: '海报',
-    field: 'img',
-     component: 'JImageUpload',
-     componentProps:{
-      },
-  },
-  {
-    label: '行程标题',
-    field: 'journeyTitle',
-    component: 'Input',
+    field: 'imgString',
+    component: 'JImageUpload',
+    componentProps: {},
   },
   {
     label: '行程描述',
     field: 'journeyDesc',
     component: 'JEditor',
   },
-	// TODO 主键隐藏字段，目前写死为ID
-	{
-	  label: '',
-	  field: 'id',
-	  component: 'Input',
-	  show: false
-	},
+  // TODO 主键隐藏字段，目前写死为ID
+  {
+    label: '',
+    field: 'id',
+    component: 'Input',
+    show: false,
+  },
 ];
 
-
-
 /**
-* 流程表单调用这个方法获取formSchema
-* @param param
-*/
-export function getBpmFormSchema(_formData): FormSchema[]{
+ * 流程表单调用这个方法获取formSchema
+ * @param param
+ */
+export function getBpmFormSchema(_formData): FormSchema[] {
   // 默认和原始表单保持一致 如果流程中配置了权限数据，这里需要单独处理formSchema
   return formSchema;
 }
--- src/views/product/Product_menu_insert.sql
+++ src/views/product/Product_menu_insert.sql
@@ -3,24 +3,24 @@
 
 
 INSERT INTO sys_permission(id, parent_id, name, url, component, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_route, is_leaf, keep_alive, hidden, hide_tab, description, status, del_flag, rule_flag, create_by, create_time, update_by, update_time, internal_or_external) 
-VALUES ('2023061403353860260', NULL, '旅游产品表', '/product/productList', 'product/ProductList', NULL, NULL, 0, NULL, '1', 0.00, 0, NULL, 1, 0, 0, 0, 0, NULL, '1', 0, 0, 'admin', '2023-06-14 15:35:26', NULL, NULL, 0);
+VALUES ('2023061703334360450', NULL, '旅游产品表', '/product/productList', 'product/ProductList', NULL, NULL, 0, NULL, '1', 0.00, 0, NULL, 1, 0, 0, 0, 0, NULL, '1', 0, 0, 'admin', '2023-06-17 15:33:45', NULL, NULL, 0);
 
 -- 权限控制sql
 -- 新增
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023061403353860261', '2023061403353860260', '添加旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:add', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-14 15:35:26', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023061703334360451', '2023061703334360450', '添加旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:add', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-17 15:33:45', NULL, NULL, 0, 0, '1', 0);
 -- 编辑
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023061403353860262', '2023061403353860260', '编辑旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:edit', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-14 15:35:26', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023061703334360452', '2023061703334360450', '编辑旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:edit', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-17 15:33:45', NULL, NULL, 0, 0, '1', 0);
 -- 删除
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023061403353860263', '2023061403353860260', '删除旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:delete', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-14 15:35:26', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023061703334360453', '2023061703334360450', '删除旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:delete', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-17 15:33:45', NULL, NULL, 0, 0, '1', 0);
 -- 批量删除
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023061403353860264', '2023061403353860260', '批量删除旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:deleteBatch', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-14 15:35:26', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023061703334360454', '2023061703334360450', '批量删除旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:deleteBatch', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-17 15:33:45', NULL, NULL, 0, 0, '1', 0);
 -- 导出excel
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023061403353860265', '2023061403353860260', '导出excel_旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:exportXls', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-14 15:35:26', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023061703334360455', '2023061703334360450', '导出excel_旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:exportXls', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-17 15:33:45', NULL, NULL, 0, 0, '1', 0);
 -- 导入excel
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023061403353860266', '2023061403353860260', '导入excel_旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:importExcel', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-14 15:35:26', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023061703334360456', '2023061703334360450', '导入excel_旅游产品表', NULL, NULL, 0, NULL, NULL, 2, 'product:product:importExcel', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-06-17 15:33:45', NULL, NULL, 0, 0, '1', 0);
