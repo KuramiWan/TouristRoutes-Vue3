b08b762bc2e7e824e2e256ae8d38deb7450cfc5b
Mingyu <1915290352@qq.com>
2023-06-20 13:52:40 +0800

    暂存

--- src/views/product/ProductList.vue
+++ src/views/product/ProductList.vue
@@ -17,7 +17,8 @@
       </template>
       <template v-else-if="column.key === 'tags'">
         <span>
-          <a-tag v-for="tag in record.tags" :key="tag" :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'">
+          <a-tag v-for="tag in record.tags" :key="tag"
+            :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'">
             {{ tag.toUpperCase() }}
           </a-tag>
         </span>
@@ -38,75 +39,77 @@
   </a-table>
 </template>
 <script>
-  import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
-  import { defineComponent, ref } from 'vue';
-  const data = [
-    {
-      key: '1',
-      productDec: 'John Brown',
-      title: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-    },
-  ];
-  export default defineComponent({
-    components: {
-      SmileOutlined,
-      DownOutlined,
-    },
-    setup() {
-      const columns = ref([
-        // {
-        //   dataIndex: 'title',
-        //   key: 'title',
-        //   resizable: true,
-        //   width: 150,
-        // },
-        {
-          title: '标题',
-          dataIndex: 'title',
-          key: 'title',
-          resizable: true,
-          width: 100,
-          minWidth: 100,
-          maxWidth: 200,
-        },
-        {
-          title: '描述',
-          dataIndex: 'productDec',
-          key: 'productDec',
-        },
-        {
-          title: '目的地',
-          key: 'departure',
-          dataIndex: 'departure',
-        },
-        {
-          title: '海报',
-          key: 'img',
-        },
-      ]);
-      return {
-        data,
-        columns,
-        handleResizeColumn: (w, col) => {
-          col.width = w;
-        },
-      };
-    },
-  });
+import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+import ProductOperate from './components/ProductOperate.vue';
+const data = [
+  {
+    key: '1',
+    productDec: 'John Brown',
+    title: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+export default defineComponent({
+  components: {
+    SmileOutlined,
+    DownOutlined,
+    ProductOperate
+  },
+  setup() {
+    const columns = ref([
+      // {
+      //   dataIndex: 'title',
+      //   key: 'title',
+      //   resizable: true,
+      //   width: 150,
+      // },
+      {
+        title: '标题',
+        dataIndex: 'title',
+        key: 'title',
+        resizable: true,
+        width: 100,
+        minWidth: 100,
+        maxWidth: 200,
+      },
+      {
+        title: '描述',
+        dataIndex: 'productDec',
+        key: 'productDec',
+      },
+      {
+        title: '目的地',
+        key: 'departure',
+        dataIndex: 'departure',
+      },
+      {
+        title: '海报',
+        key: 'img',
+      },
+    ]);
+    return {
+      data,
+      columns,
+      handleResizeColumn: (w, col) => {
+        col.width = w;
+      },
+    };
+  },
+});
 </script>
---
+++ src/views/product/components/ProductOperate.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <a-button type="primary" @click="showModal">{{ name }}</a-button>
+        <a-modal v-model:visible="visible" title="Basic Modal" @ok="handleOk">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </a-modal>
+    </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+    props: ['name'],
+    setup() {
+        const visible = ref(false);
+        const showModal = () => {
+            visible.value = true;
+        };
+        const handleOk = e => {
+            console.log(e);
+            visible.value = false;
+        };
+        return {
+            visible,
+            showModal,
+            handleOk,
+        };
+    },
+});
+</script>
