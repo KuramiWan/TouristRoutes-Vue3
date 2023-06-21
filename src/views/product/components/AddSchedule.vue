6bc3bac1f4887a76794667de7d0cd92a1461b705
BooleanChar12 <2507260744@qq.com>
2023-06-21 22:03:37 +0800

    暂存

--- src/views/product/components/AddSchedule.vue
+++ src/views/product/components/AddSchedule.vue
@@ -1,37 +1,37 @@
 <template>
-    <div>
-        <a @click="showModal">{{ name }}</a>
-        <a-modal v-model:visible="visible" title="Title" :confirm-loading="confirmLoading" @ok="handleOk">
-            <p>{{ modalText }}</p>
-        </a-modal>
-    </div>
+  <div>
+    <a @click="showModal">{{ name }}</a>
+    <a-modal v-model:visible="visible" title="Title" :confirm-loading="confirmLoading" @ok="handleOk">
+      <p>{{ modalText }}</p>
+    </a-modal>
+  </div>
 </template>
 <script>
-import { ref, defineComponent } from 'vue';
-export default defineComponent({
+  import { ref, defineComponent } from 'vue';
+  export default defineComponent({
     props: ['name'],
     setup() {
-        const modalText = ref('Content of the modal');
-        const visible = ref(false);
-        const confirmLoading = ref(false);
-        const showModal = () => {
-            visible.value = true;
-        };
-        const handleOk = () => {
-            modalText.value = 'The modal will be closed after two seconds';
-            confirmLoading.value = true;
-            setTimeout(() => {
-                visible.value = false;
-                confirmLoading.value = false;
-            }, 2000);
-        };
-        return {
-            modalText,
-            visible,
-            confirmLoading,
-            showModal,
-            handleOk,
-        };
+      const modalText = ref('Content of the modal');
+      const visible = ref(false);
+      const confirmLoading = ref(false);
+      const showModal = () => {
+        visible.value = true;
+      };
+      const handleOk = () => {
+        modalText.value = 'The modal will be closed after two seconds';
+        confirmLoading.value = true;
+        setTimeout(() => {
+          visible.value = false;
+          confirmLoading.value = false;
+        }, 2000);
+      };
+      return {
+        modalText,
+        visible,
+        confirmLoading,
+        showModal,
+        handleOk,
+      };
     },
-});
+  });
 </script>
